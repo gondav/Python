e85a9217f9f5924dff061e62b40179dcd05a80d6
gondav <65767150+gondav@users.noreply.github.com>
2020-09-21 16:14:06 +0200

    Add files via upload
    Homework1

---
+++ pay1.py
@@ -0,0 +1,11 @@
+hours = input("Enter hours: ")
+rate = input("Enter rate: ")
+
+hours1 = float(hours)
+rate1 = float(rate)
+
+pay = hours1 * rate1
+
+print(f"Pay: {round(pay, 2)}")
+
+input("prompt:")
